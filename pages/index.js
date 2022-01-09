c339d45a0c2c76e11ca75b9fd2ed134e3d43830b
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2022-01-09 11:36:02 -0300

    feat: tailwind config and hidden json added

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-green-500 font-bold text-5xl">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
